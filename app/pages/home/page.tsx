a7966a01f007644772a286ecf584ffe586901007
sharkfacekilla <sharkfacekilla@gmail.com>
2024-06-16 17:25:30 -0700

    hide navbar on scroll down

--- app/pages/home/page.tsx
+++ app/pages/home/page.tsx
@@ -5,10 +5,10 @@ const HomePage = () => {
     return (
         <>
             <section>
-                <div className="container mx-auto px-4">
-                    <h1 className="mx-auto mt-32 mb-5 font-extrabold tracking-tight md:text-8xl sm: text-5xl dark:text-light-blue">Gage Patenaude.</h1>
+                <div className="container mt-28 mx-auto px-4">
+                    <h1 className="mx-auto mb-5 font-extrabold tracking-tight md:text-8xl sm: text-5xl dark:text-light-blue">Gage Patenaude.</h1>
                     <h2 className="mx-auto mb-5 font-extrabold tracking-tight md:text-6xl sm: text-3xl dark:text-white/50">Building cool things.</h2>
-                    <div className="w-full h-screen">
+                    <div className="w-full h-96 mt-8">
                         <p className="max-w-3xl">
                             Hello, and welcome to my portfolio!
                         </p>
@@ -16,7 +16,7 @@ const HomePage = () => {
                         <p className="max-w-3xl">
                             Learn a bit more about me and my projects here!
                         </p>
-                        <Button variant="outlined">Yeet</Button>
+                        <Button variant="outlined" className="mt-5">Yeet</Button>
                     </div>
                 </div>
             </section>
