cff008ebb19fcc54d54f6c8bff26666254f55506
sharkfacekilla <sharkfacekilla@gmail.com>
2024-06-17 23:12:28 -0700

    changed some padding

--- app/pages/home/page.tsx
+++ app/pages/home/page.tsx
@@ -4,11 +4,11 @@ import React from "react";
 const HomePage = () => {
     return (
         <>
-            <section>
-                <div className="container mt-28 mx-auto px-4">
-                    <h1 className="mx-auto mb-5 font-extrabold tracking-tight md:text-8xl sm: text-5xl dark:text-light-blue">Gage Patenaude.</h1>
+        <section className="h-2/6">
+        <div className="container mx-auto px-4 mb-20">
+        <h1 className="mx-auto mb-5 font-extrabold tracking-tight md:text-8xl sm: text-5xl dark:text-light-blue mt-32">Gage Patenaude.</h1>
                     <h2 className="mx-auto mb-5 font-extrabold tracking-tight md:text-6xl sm: text-3xl dark:text-white/50">Building cool things.</h2>
-                    <div className="w-full h-96 mt-8">
+                    <div>
                         <p className="max-w-3xl">
                             Hello, and welcome to my portfolio!
                         </p>
@@ -16,8 +16,29 @@ const HomePage = () => {
                         <p className="max-w-3xl">
                             Learn a bit more about me and my projects here!
                         </p>
-                        <Button variant="outlined" className="mt-5">Yeet</Button>
+                        
+                    <Button href={"pages/about"} variant="contained" className="mt-4 bg-blue text-white hover:bg-light-blue border">About Me</Button>
                     </div>
+                </div>
+                </section>
+                <section className=" h-3/6">
+                <div className="container mx-auto mb-80 px-4 h-full grid grid-cols-2 grid-rows-2 flex items-center justify-center">
+                    <h1 className="text-white text-4xl font-bold z-10 py-4 col-span-full mx-auto mb-5 border-b">Recent Project</h1>
+                    <p className="text-white">
+                        I am an aspiring computer scientist who enjoys a new
+                        challenges: be it learning something new, or solving an
+                        issue that people before me have tried. I have strong
+                        technical skills and an academic background in
+                        programming, web building and design. My passion lies in
+                        having that "a-ha" moment when solving problems, using
+                        tools that I have learned how to use in my academic and
+                        professional journey. In my studies, I've worked in a
+                        team environment, as well as on my own, creating
+                        different project. I'm graduating in 2025, and
+                        interested in a full-time development career. I also
+                        have interests in music, and photography. I have listed
+                        some of my personal passions below.
+                    </p>
                 </div>
             </section>
         </>
