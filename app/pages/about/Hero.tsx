bf0b0febfaae641e2904c97154d6812321c6903b
sharkfacekilla <sharkfacekilla@gmail.com>
2024-06-17 23:47:16 -0700

    work on about

--- app/pages/about/Hero.tsx
+++ app/pages/about/Hero.tsx
@@ -6,18 +6,7 @@ const AboutMe = () => {
             <section className=" h-screen/2 bg-gradient-to-r from-black to-transparent">
                 <div className=" inset-0 h-screen/2 bg-aboutMe h-svh">
                     <div className="container grid grid-cols-2 content-center grid-rows-2 mx-auto px-4 h-full flex items-center justify-center">
-                        <h1 className="text-white text-4xl font-bold z-10 mt-auto py-4 col-span-full">About Me</h1>
-                        <p className="mb-auto">
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing
-                            elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua. Ut enim ad minim veniam, quis
-                            nostrud exercitation ullamco laboris nisi ut aliquip
-                            ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore
-                            eu fugiat nulla pariatur. Excepteur sint occaecat
-                            cupidatat non proident, sunt in culpa qui officia
-                            deserunt mollit anim id est laborum."
-                        </p>
+                        <h1 className="text-white font-extrabold md:text-8xl text-5xl sm:text-3xl text-light-blue z-10 mt-auto py-4 col-span-full">Who Am I?</h1>
                     </div>
                 </div>
             </section>
