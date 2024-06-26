464638832cb857567deab39ee935779ff41054ed
sharkfacekilla <sharkfacekilla@gmail.com>
2024-06-25 20:01:57 -0700

    some work on page working on img

--- app/pages/home/page.tsx
+++ app/pages/home/page.tsx
@@ -3,16 +3,12 @@ import React from "react";
 import RecentProjectCard from '../../components/RecentProjectCard';
 import { Project } from '../../utils/project';
 
-interface CustomCardProps {
-    project: Project;
-}
-
-const HomePage: React.FC<CustomCardProps> = () => {
+const HomePage = () => {
     return (
         <>
-        <section className="h-2/6">
-        <div className="container mx-auto px-4 mb-20">
-        <h1 className="mx-auto mb-5 font-extrabold tracking-tight md:text-8xl sm: text-5xl dark:text-light-blue mt-32">Gage Patenaude.</h1>
+            <section className="h-2/6">
+                <div className="container mx-auto px-4 mb-20">
+                    <h1 className="mx-auto mb-5 font-extrabold tracking-tight md:text-8xl sm: text-5xl dark:text-light-blue mt-32">Gage Patenaude.</h1>
                     <h2 className="mx-auto mb-5 font-extrabold tracking-tight md:text-6xl sm: text-3xl dark:text-white/50">Building cool things.</h2>
                     <div>
                         <p className="max-w-3xl">
@@ -22,21 +18,20 @@ const HomePage: React.FC<CustomCardProps> = () => {
                         <p className="max-w-3xl">
                             Learn a bit more about me and my projects here!
                         </p>
-                    <Button href={"pages/about"} variant="contained" className="mt-4 bg-blue text-white hover:bg-light-blue border">About Me</Button>
+                        <Button href={"pages/about"} variant="contained" className="mt-4 bg-blue text-white hover:bg-light-blue border">About Me</Button>
                     </div>
                 </div>
                 <div className="container mx-auto mb-5 px-4 h-full flex items-center justify-center">
                     <h1 className="text-white text-4xl font-bold z-10 py-4 col-span-full mx-auto mb-5 border-b">Recent Project</h1>
                 </div>
                 <div className="container mx-auto mb-10 px-4 h-full flex items-center justify-center">
-
                     <Grid container spacing={2} justifyContent="center" alignItems="center">
-                            <Grid item>
-                                <RecentProjectCard proj="1" />
-                            </Grid>
+                        <Grid item>
+                            <RecentProjectCard proj="1" />
+                        </Grid>
                     </Grid>
-                    </div>
-                </section>
+                </div>
+            </section>
         </>
     );
 };
--- app/pages/projects/[id]/page.tsx
+++ app/pages/projects/[id]/page.tsx
@@ -9,22 +9,17 @@ export default function ProjectPage({params} : {
     const projectData = data.projects.find((p) => p.id === params.id );
 
     return (
-        <div className="relative h-screen">
-          <section className="h-3/5 relative">
-            <img src={projectData?.img} className="absolute inset-0 w-full h-full object-cover opacity-90" alt="Background" />
-            <div className="container mx-auto px-4 flex h-full">
-            <h1 className="text-white text-5xl font-bold z-10 py-4 mt-20 col-span-full mx-auto mb-5 text-shadow">
-            {projectData?.name}
-              </h1>
-            </div>
-          </section>
-          <section className="h-2/5">
-            <div className="container mx-auto px-4 flex items-center justify-center h-full">
-              <div className="text-white text-3xl font-bold">
-                Content goes here
-              </div>
-            </div>
-          </section>
-        </div>
-      );
-    };      
+        <>
+            <section className="h-screen">
+                <div className='container'>
+                    <div className="relative">
+                    <img src={projectData?.img} className="absolute inset-0 w-full h-full object-cover opacity-90" alt="Background" />
+                        </div>
+                
+                    <h1 className="text-white text-4xl text-center text-shadow font-bold z-10 py-4 col-span-full mx-auto mb-5">{projectData?.name}</h1>
+                    
+                </div>
+            </section>
+        </>
+    );
+};      
