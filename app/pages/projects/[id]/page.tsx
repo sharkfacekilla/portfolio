db6fd0aaa57a6c2f946db7829d0d1354185b8700
sharkfacekilla <sharkfacekilla@gmail.com>
2024-07-06 16:19:13 -0700

    finished project page layout

--- app/pages/projects/[id]/page.tsx
+++ app/pages/projects/[id]/page.tsx
@@ -10,16 +10,30 @@ export default function ProjectPage({params} : {
 
     return (
         <>
-            <section className="h-screen">
-                <div className='container'>
-                    <div className="relative">
-                    <img src={projectData?.img} className="absolute inset-0 w-full h-full object-cover opacity-90" alt="Background" />
+            <section className="h-screen relative">
+                <div className="absolute inset-0 w-full h-screen">
+                    <img src={projectData?.img} className="w-full h-full object-cover" alt="Background" />
+                    <div className="absolute inset-0 bg-black opacity-50"></div>
+                </div>
+                <div className="relative container text-center mx-auto h-1/2 flex items-center justify-center">
+                    <h1 className="text-white text-5xl text-center text-shadow font-bold z-10 py-4 mx-auto mb-5">{projectData?.name}</h1>
+                </div>
+            </section>
+            <section className="h-screen relative">
+                <div className="absolute inset-0 w-full">
+                    <div className="mt-10 relative container text-center mx-auto h-1/2 items-center justify-center">
+                        <h2 className="text-white text-3xl text-center text-shadow font-bold z-10 py-4 mx-auto mb-5">Project Description</h2>
+                        <p className="mb-10">{projectData?.description}</p>
+                        <p className="mb-10 -mt-5">Languages and Tools used: {projectData?.lang}</p>
+                        <div>
+                            <h3 className="text-white text-2xl text-center text-shadow font-bold z-10 py-4 mx-auto">Demo Video</h3>
+                            <div className="justify-center items-center mt-10">
+                                <iframe width="560" height="315" src={projectData?.link} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" className="justify-center items-center mx-auto"></iframe>
+                            </div>
                         </div>
-                
-                    <h1 className="text-white text-4xl text-center text-shadow font-bold z-10 py-4 col-span-full mx-auto mb-5">{projectData?.name}</h1>
-                    
+                    </div>
                 </div>
             </section>
         </>
     );
-};      
+};
