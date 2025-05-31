003fef6a4dda2c2cbd38c11a97b9390e1a98c27a
sharkfacekilla <sharkfacekilla@gmail.com>
2025-05-31 13:21:09 -0700

    Created file to hold types instead of re-defining every time needed

---
+++ app/utils/app-types.ts
@@ -0,0 +1,45 @@
+export type Feature = {
+  name: string;
+  description: string;
+};
+
+export type User = {
+  name: string;
+  description: string;
+};
+
+export type Project = {
+  id: string;
+  name: string;
+  shortDesc: string;
+  img: string;
+  description: string;
+  link: string;
+  youtubeLink: string;
+  githubLink: string;
+  tech: TechCategory[];
+  altTxt: string;
+  skills: { [key: string]: string | undefined };
+  status: string;
+  timeline: string;
+  team: string;
+  features?: Feature[];
+  challenges?: string[];
+  learnings?: string[];
+  goals?: string[];
+  users?: User[];
+};
+
+export type TechCategory = {
+    name: string;
+    technology: string[];
+};
+
+export type ProjectProps = {
+  project: Project;
+};
+
+export type ProjectParams = {
+  params: { id: string };
+  isVisible: boolean;
+}
